596dc3c09344c8f86f008c3b6665452a04601054
thipham2018 <trangphamtmp@gmail.com>
2024-02-01 11:38:52 -0500

    add services

--- Services/JsonFileProductsService.cs
+++ Services/JsonFileProductsService.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using WebApp.Models;
 
 namespace WebApp.Services
 {
@@ -18,5 +21,18 @@ namespace WebApp.Services
         {
             get { return System.IO.Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+
+        }
+
     }
 }
