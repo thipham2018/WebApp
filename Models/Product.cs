12b151d64eb8b2f5f036bf1ea476036b94195e72
thipham2018 <trangphamtmp@gmail.com>
2024-02-01 10:58:25 -0500

    set variable for data

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,9 +13,7 @@ namespace WebApp.Models
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
-
-
+        public int[] Ratings { get; set; }
 
     }
 }
