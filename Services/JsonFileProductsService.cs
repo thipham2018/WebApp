33dc64dd0b03c9356f8fc3afbf89d7e8911c67ca
thipham2018 <trangphamtmp@gmail.com>
2024-02-02 10:15:30 -0500

    new service

--- Services/JsonFileProductsService.cs
+++ Services/JsonFileProductsService.cs
@@ -15,15 +15,15 @@ namespace WebApp.Services
         {
             WebHostEnvironment = webHostEnvironment;
         }
-        public IWebHostEnvironment WebHostEnvironment { get; 
-        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
         private string JsonFileName
         {
-            get { return System.IO.Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
         public IEnumerable<Product> GetProducts()
         {
-            using(var jsonFileReader = File.OpenText(JsonFileName))
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
