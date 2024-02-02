da856b5b8447b793215513522cfd5cc1f1923f4b
thipham2018 <trangphamtmp@gmail.com>
2024-02-02 10:18:22 -0500

    new service

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddTransient<JsonFileProductsService>()
+            services.AddTransient<JsonFileProductsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
