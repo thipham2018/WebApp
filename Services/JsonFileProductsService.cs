5fd3714d7db8d1ad3a0ab483b8c3fa01b36d87a2
thipham2018 <trangphamtmp@gmail.com>
2024-02-14 13:30:15 -0500

    add rating to jsonfileproductsService

--- Services/JsonFileProductsService.cs
+++ Services/JsonFileProductsService.cs
@@ -38,7 +38,20 @@ namespace WebApp.Services
         public void AddRating(string productId, int rating)
         {
             var products = GetProducts();
+
+            //LINQ
             var query = products.First(x => x.Id == productId);
+
+            if (query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
 
         }
     }
